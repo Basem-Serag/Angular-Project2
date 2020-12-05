28a2e4c2fa716e7621ba958411d98ed19a4ac98f
Basem_Serag <basemserag.911@gmail.com>
2020-12-05 02:18:14 +0200

    [Bug] some errors fixed

--- src/app/Components/not-found/not-found.component.ts
+++ src/app/Components/not-found/not-found.component.ts
@@ -1,6 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
-import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import { FormGroup, FormBuilder , FormControl } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
 
 export interface Subject {
   name: string;
@@ -13,53 +11,13 @@ export interface Subject {
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss']
 })
-export class NotFoundComponent {
-
-  visible = true;
-  selectable = true;
-  removable = true;
-  addOnBlur = true;
-  myForm: FormGroup;
-
-  GradeArray: any = ['8th Grade', '9th Grade', '10th Grade', '11th Grade', '12th Grade'];
-  SubjectsArray: Subject[] = [];
+export class NotFoundComponent implements OnInit{
 
 
-  constructor(public fb: FormBuilder) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.reactiveForm()
-  }
-
-  /* Reactive form */
-  reactiveForm() {
-    this.myForm = this.fb.group({
-      name: [''],
-      email: [''],
-      gender: ['Male'],
-      dob: [''],      
-      grade: [''],
-      subjects: [this.SubjectsArray]
-    })
-  }
-
-  'newForm' = new FormGroup({
-    id: new FormControl(),
-    user: new FormControl()
-  })
+  ngOnInit(): void {  }
 
 
-
-  submitForm() {
-    console.log(this.myForm.value)
-    localStorage.setItem('USER' , JSON.stringify( this.myForm.value))
-  }
-
-  anotherForm(){
-    console.log(this.myForm.value + 'hello')
-    localStorage.setItem('NEW_USER' , JSON.stringify( this.myForm.value))
-    
-
-  }
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { NotFoundComponent } from './Components/not-found/not-found.component';
 import { SignUpComponent } from './Components/sign-up/sign-up.component';
 import { SignInComponent } from './Components/sign-in/sign-in.component';
 import { ProfileComponent } from './Components/profile/profile.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './Guards/auth.guard';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
 
 const routes: Routes = [
   {path: '' , redirectTo:'signin' , pathMatch:'full'},
